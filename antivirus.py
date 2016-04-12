e71ce5e7ad40d9fde8788d31f6a070da5ba45e3a
Owen Donckers <odonckers@gmail.com>
2016-04-12 18:42:03 -0500

    In this commit I was able to make it where you can scan differant directorys, but I need to make it where I can open a directory and find another directory and go inside it.

--- antivirus.py
+++ antivirus.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python
 import lib
+import sys
 
-menu = "Antivirus 1.0 Snake Engine\n[a] Start Scan\nSelection: "
-select = raw_input(menu)
-if select == "a":
-        lib.scan()
+while True:
+	menu = "Antivirus 1.0 Snake\n[a] Start Scan\n[x] Exit\nSelection: "
+	select = raw_input(menu)
+
+	if select == "a":
+		filename = ""
+		while filename == "":
+			filename = raw_input( "File/Directory to Scan: " )
+		lib.scan(filename, "/Users/odonckers/Documents/Hacking/Python/Snake-Software/data/virsig.cfg")
+		continue
+	elif select == "x":
+		sys.exit(0)
+	else:
+		print "Incorrect selection"
+		continue
--- lib/scan.py
+++ lib/scan.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python
+import re
+import os
 
-def scan():
-	safe = 0
-
-	filename = ""
-	while filename == "":
-		filename = raw_input( "File/Directory to Scan: " )
-
-	for line in open( "data/virsig.cfg", "r" ):
-		virname, virsig = line.split('=')
-
+def scan(filename, virsigdes):
+	if os.path.isdir(filename) == True:
+		safedir = 0
+		os.chdir(filename)
+		for fileline in os.listdir(os.getcwd()):
+			if os.path.isdir(fileline) == True:
+				print '\033[94m' + "[Can't Open Directorys]" + '\033[0m' + " %s" % fileline
+			elif os.path.exists(fileline) == True: 
+				filecontent = open(fileline)
+				for line in open(virsigdes):
+					virname, virsigs = line.split('=')
+					if filecontent == re.match('\Q\E\s*', virsigs):
+						print '\033[91m' + "[Infected by %s]" % virname + '\033[0m' + " %s" % fileline
+					elif filecontent != re.match('\Q\E\s*', virsigs):
+						safedir += 1
+						if safedir == 1781:
+							print '\033[92m' + "[Clean]" + '\033[0m' + " %s" % fileline 
+	elif os.path.exists(filename) == True:
+		safefile = 0
 		for fileline in open(filename):
-			if fileline == virsig:
-				print '\033[91m' + "[Infected by %s]" + '\033[0m' + " %s" % (virname, filename)
-			else:
-				safe += 1
-				if safe == 1781:
-					print '\033[92m' + "[Clean]" + '\033[0m' + " %s" % filename
+			for line in open("data/virsig.cfg", "r"):
+				virname, virsigs = line.split('=')
+				if fileline == re.match('\Q\E\s*', virsigs):
+					print '\033[91m' + "[Infected by %s]" + '\033[0m' + " %s" % (virname, filename)
+				elif fileline != re.match('\Q\E\s*', virsigs):
+					safefile += 1
+					if safefile == 1781:
+						print '\033[92m' + "[Clean]" + '\033[0m' + " %s" % filename
+	else:
+		print '\033[94m' + "[Can't Open]" + '\033[0m' + " %s" % filename
