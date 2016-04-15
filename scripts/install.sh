33f319d4d2f6bf3367acbb575a0830d9ccfedc27
Owen Donckers <odonckers@gmail.com>
2016-04-15 16:11:20 -0500

    I edited the name of a variable in scripts/install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 echo "Specify destination path for Snake Antivirus: "
 read path
-dest = pwd
+destination=pwd
 if [[ -d $path ]]; then
     nowpath=`pwd`
     cd $path
@@ -10,7 +10,7 @@ if [[ -d $path ]]; then
     cd $nowpath
     cp -av "../lib/" "$path/SnakeAntivirus/lib"
     cp -av "../data/" "$path/SnakeAntivirus/data"
-    echo "virsig.cfg_path=$dest/data/virsig.cfg\nlog_path=$dest/data/event.log\nvirsig_database_strings=http://www.nlnetlabs.nl/downloads/antivirus/antivirus/virussignatures.strings" >> ../data/config.cfg
+    echo "virsig.cfg_path=$destination/data/virsig.cfg\nlog_path=$destination/data/event.log\nvirsig_database_strings=http://www.nlnetlabs.nl/downloads/antivirus/antivirus/virussignatures.strings" >> ../data/config.cfg
     cp -av "../antivirus.py" "$path/SnakeAntivirus/"
     cp -av "run.sh" "$path/SnakeAntivirus/"
     echo "Snake Antivirus was installed"
